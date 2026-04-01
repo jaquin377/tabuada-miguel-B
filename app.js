27d7ed8ab7e1d38f893e6fd9b157c29e324fc51d
gabriel-tech1314 <gabriel.7344@colegioprogresso.g12.br>
2026-04-01 09:27:31 -0300

    finalizado

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const select = document.getElementById("select")
-const button = document.getElementById("button")
+const button = document.getElementById("button") 
+const resultado = document.getElementById("resultado")
 
-function gerarTabuada(){
+            function gerarTabuada(){
     const tabuada = select.value
     let n = 0 
     while(n <= 10){
-        console.log(tabuada = n)
+        resultado.innerHTML += `${tabuada} x ${n} = ${tabuada * n}<br>` 
         n++
+
+        
     }
 
 }
